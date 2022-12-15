c68d9b1774d3d993e6887d7757cdfb156f4cff9b
Nooruldeen <smartinreal@gmail.com>
2022-12-15 12:45:09 +0300

    delete-Done

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/dashboard', function () {
 
 Route::resource('chirps', ChirpController::class)
     // ->only(['index', 'store'])
-    ->only(['index', 'store', 'edit', 'update'])
+    ->only(['index', 'store', 'edit', 'update','destroy'])
     ->middleware(['auth', 'verified']);
 // Route::resource('/chirps/index',ChirpController::class)->name('index')->middleware(['auth', 'verified']);
 
